1a61b26bcfdc7b085dc8b865819d0f3f67775ac4
vudangkhoa11 <khoavdph32996@fpt.edu.vn>
2024-03-06 16:01:51 +0700

    Mô tả thay đổi

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include "view/header.php";
+    if(isset($_GET['act'])&&($_GET['act']!="")){
+        $act=$_GET['act'];
+        switch($act){
+            case "chitietsanpham":
+                include "view/chitietsanpham.php";
+                break;
+        }
+    }else{
+        include "view/home.php";
+    }
+   
+    include "view/footer.php";
+?>
+
+        
